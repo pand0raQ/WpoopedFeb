4edfccfca4de6dafebbe5bae2c7acca83adb5984
pand0raQ <pand0ra_Q@hotmail.com>
2025-02-07 18:44:49 +0400

    Initial Commit

---
+++ WpoopedFeb/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WpoopedFeb
+//
+//  Created by Halik on 2/7/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ WpoopedFeb/WpoopedFebApp.swift
@@ -0,0 +1,32 @@
+//
+//  WpoopedFebApp.swift
+//  WpoopedFeb
+//
+//  Created by Halik on 2/7/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WpoopedFebApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
